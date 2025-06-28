856be49d330c80cff8a841b286a75991e0914935
sid314 <sidaa314@gmail.com>
2025-06-28 12:45:35 +0530

    Animation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from manim import (
     PINK,
     Axes,
+    Create,
+    FadeIn,
     Scene,
 )
 
@@ -10,4 +12,7 @@ class Test(Scene):
         ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
         curve = ax.plot(lambda x: (x) * (x - 2) * (x + 2), color=PINK)
         area = ax.get_area(curve, x_range=(-2, 0))
-        self.add(ax, curve, area)
+        self.play(Create(ax, run_time=3), Create(curve, run_time=5))
+        self.play(FadeIn(area))
+
+        self.wait()
