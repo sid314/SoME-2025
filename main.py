b5c1e1f6bf5bc2987a3c693301155b53de9db99e
sid314 <sidaa314@gmail.com>
2025-06-28 12:52:42 +0530

    More Animations
    Transform,Indicate

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from manim import (
-    PINK,
-    Axes,
-    Create,
-    FadeIn,
+    BLUE,
+    GREEN,
+    Circle,
+    DrawBorderThenFill,
+    FadeOut,
+    Indicate,
+    ReplacementTransform,
     Scene,
+    Square,
 )
 
 
 class Test(Scene):
     def construct(self):
-        ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
-        curve = ax.plot(lambda x: (x) * (x - 2) * (x + 2), color=PINK)
-        area = ax.get_area(curve, x_range=(-2, 0))
-        self.play(Create(ax, run_time=3), Create(curve, run_time=5))
-        self.play(FadeIn(area))
-
-        self.wait()
+        green_square = Square(color=GREEN, fill_opacity=0.5)
+        self.play(DrawBorderThenFill(green_square))
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        self.play(ReplacementTransform(green_square, blue_circle))
+        self.play(Indicate(blue_circle))
+        self.play(FadeOut(blue_circle))
