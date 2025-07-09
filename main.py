a3c3a8a13d760d6fbe3b4c5edbb40e96b22e395a
sid314 <sidaa314@gmail.com>
2025-07-09 20:02:08 +0530

    Animated mosquito example

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from manim import (
+    DARK_BLUE,
+    DEGREES,
+    DOWN,
     GREEN,
+    IN,
+    LEFT,
     ORANGE,
+    OUT,
     RED,
     RIGHT,
     UP,
+    Create,
+    Cube,
     Dot,
+    Dot3D,
+    DrawBorderThenFill,
+    FadeOut,
     NumberPlane,
+    Point,
     Scene,
     Square,
+    ThreeDAxes,
+    ThreeDScene,
+    TracedPath,
 )
 
 
@@ -24,3 +39,28 @@ class Test(Scene):
         s = Square(color=ORANGE)
         s.shift(2 * UP + 4 * RIGHT)
         self.add(s)
+
+
+class MosquitoCube(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=75 * DEGREES, theta=90 * DEGREES)
+        axes = ThreeDAxes()
+
+        self.begin_ambient_camera_rotation(rate=0.1)
+        self.add(axes)
+
+        cube = Cube(fill_opacity=0.25, stroke_width=1.0, side_length=4)
+        self.play(DrawBorderThenFill(cube))
+        self.stop_ambient_camera_rotation()
+        self.wait(1)
+        dot_a = Dot3D(color=DARK_BLUE, radius=0.045)
+        corner_b = Point((DOWN + RIGHT + OUT) * cube.side_length / 2)
+        corner_a = Point((UP + LEFT + IN) * cube.side_length / 2)
+        dot_a.move_to(corner_a)
+        self.play(Create(dot_a))
+        dotPath = TracedPath(dot_a.get_center, stroke_color=RED)
+        self.add(dotPath)
+        self.play(dot_a.animate.move_to(corner_b))
+        self.play(FadeOut(dot_a, dotPath))
+
+        self.wait(5)
